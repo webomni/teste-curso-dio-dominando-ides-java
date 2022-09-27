72aa798864f8141149c0131c9cf4a80fa9d6c13b
webomni <prpaulohcmg@hotmail.com>
2022-09-26 23:07:03 -0300

    Formatação

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -6,42 +6,40 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		double a;
 		int b;
-		
+
 		System.out.print("Digite o primeiro valor: ");
 		a = scan.nextDouble();
 		System.out.print("Digite o segundo valor: ");
 		b = scan.nextInt();
-		
-		
-		double soma = soma(a,b);
-		double subtracao = subtracao(a,b);
-		 double divisao = divisao(a,b);
-		 double multiplicacao = multiplicacao(a, b);
-		
-		 System.out.println(soma);
-		 System.out.println(subtracao);
-		 System.out.println(divisao);
-		 System.out.println(multiplicacao);
+
+		double soma = soma(a, b);
+		double subtracao = subtracao(a, b);
+		double divisao = divisao(a, b);
+		double multiplicacao = multiplicacao(a, b);
+
+		System.out.println(soma);
+		System.out.println(subtracao);
+		System.out.println(divisao);
+		System.out.println(multiplicacao);
 	}
-	
+
 	public static double soma(double a, int b) {
 		return a + b;
 	}
-	
+
 	public static double subtracao(double a, int b) {
 		return a - b;
 	}
-	
+
 	public static double divisao(double a, int b) {
 		return a / b;
 	}
-	
+
 	public static double multiplicacao(double a, int b) {
 		return a * b;
 	}
-	
 
 }
